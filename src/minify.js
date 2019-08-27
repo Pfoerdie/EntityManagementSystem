11a48ce4099e7a8da8af320fd17675a1c558dbc1
Simon Petrac <pfoerdie@gmail.com>
2019-08-27 18:40:58 +0200

    wip

--- src/minify.js
+++ src/minify.js
@@ -1,23 +1,42 @@
 const
     Path = require('path'),
-    Fs = require('fs'),
-    tools = Fs.readdirSync(Path.join(__dirname, "tools")).filter(file => file.endsWith(".js") && file !== "index.js"),
-    cypher = Fs.readdirSync(Path.join(__dirname, "cypher")).filter(file => file.endsWith(".cyp") && file !== "index.js"),
-    modules = Fs.readdirSync(Path.join(__dirname, "modules")).filter(file => file.endsWith(".js") && file !== "index.js");
+    Fs = require('fs');
 
-console.log("tools:", tools);
-console.log("cypher:", cypher);
-console.log("modules:", modules);
+console.log(scanDirectory(__dirname));
 
-// TODO
+// const
+//     tools = Fs.readdirSync(Path.join(__dirname, "tools")).filter(file => file.endsWith(".js") && file !== "index.js"),
+//     cypher = Fs.readdirSync(Path.join(__dirname, "cypher")).filter(file => file.endsWith(".cyp") && file !== "index.js"),
+//     modules = Fs.readdirSync(Path.join(__dirname, "modules")).filter(file => file.endsWith(".js") && file !== "index.js");
 
-let EMSjs = `
-    Test
-`;
+// console.log("tools:", tools);
+// console.log("cypher:", cypher);
+// console.log("modules:", modules);
 
-let EMSminjs = EMSjs
-    .replace(/\s+/g, " ")
-    .trim();
+// // TODO
 
-Fs.writeFileSync(Path.join(__dirname, "EMS.js"), EMSjs);
-Fs.writeFileSync(Path.join(__dirname, "EMS.min.js"), EMSminjs);
+// let EMSjs = `
+//     Test
+// `;
+
+// let EMSminjs = EMSjs
+//     .replace(/\s+/g, " ")
+//     .trim();
+
+// Fs.writeFileSync(Path.join(__dirname, "EMS.js"), EMSjs);
+// Fs.writeFileSync(Path.join(__dirname, "EMS.min.js"), EMSminjs);
+
+function scanDirectory(directory, depth = Infinity) {
+    let content = Fs.readdirSync(directory);
+    let result = {};
+    for (let name of content) {
+        let path = Path.join(directory, name);
+        let stats = Fs.statSync(path);
+        if (stats.isDirectory()) {
+            result[name] = scanDirectory(path, depth - 1);
+        } else if (stats.isFile()) {
+            result[name] = Fs.readFileSync(path).toString();
+        }
+    }
+    return result;
+}
